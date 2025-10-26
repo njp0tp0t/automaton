c07c2c442bd98d3c529496acd2c56dab42e13371
Noel Joseph Jovero <njjovero26@gmail.com>
2025-10-26 19:47:00 -0400

    im tired

--- Automaton.java
+++ Automaton.java
@@ -52,20 +52,11 @@ public class Automaton
         // based on the state of its two neighbors.
         for(int i = 0; i < state.length; i++) {
             int left, center, right;
-            if(i == 0) {
-                left = 0;
-            }
-            else {
-                left = state[i - 1];
-            }
+            left = (i == 0) ? 0 : state[i - 1];
             center = state[i];
-            if(i + 1 < state.length) {
-                right = state[i + 1];
-            }
-            else {
-                right = 0;
-            }
+            right = (i + 1 < state.length) ? state[i + 1] : 0;
             nextState[i] = (left + center + right) % 2;
+            nextState = state;
         }
         state = nextState;
     }
